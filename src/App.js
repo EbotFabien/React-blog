ec5b7763736525c91e295b3c834217a63b316648
Ebot <touchone0001@gmail.com>
2023-12-04 00:09:24 +0100

    'done'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import  Container  from "react-bootstrap/Container";
+import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
+import Header from './components/Header';
+import FeedPage from "./pages/FeedPage";
+
+import ExplorePage from "./pages/ExplorePage";
+import UserPage from "./pages/UserPage";
+import LoginPages from "./pages/LoginPages";
+import ApiProvider from "./contexts/ApiProvider";
+
 
 function App() {
+  
+
   return (
-    <h1>Microblog</h1>
+    <Container fluid className="App">
+      <BrowserRouter>
+      <ApiProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<FeedPage/>} />
+        <Route path="/explore" element={<ExplorePage/>}/>
+        <Route path="/user/:username" element={<UserPage />} /> 
+        <Route path="/login" element={<LoginPages />} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+      </ApiProvider>
+      </BrowserRouter>
+      
+      
+        
+        
+    
+    </Container>
+    
   );
 }
 
---
+++ src/pages/LoginPages.js
@@ -0,0 +1,10 @@
+import Body from '../components/Body';
+
+export default function loginPage(){
+    return(
+        <Body>
+            <h1>Login form</h1>
+            <p>TODO</p>
+        </Body>
+    );
+}
